bdb6b2a06c984b730b16a8096c6e5b3317d26f03
IJustDaniii <danielgameromartinezzz@gmail.com>
2026-09-01 00:26:35 +0200

    limpieza

--- EDITAR-DISENO.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Personalizar diseno de OKEY SUITES
-cd /d "%~dp0"
-where code >nul 2>nul
-if errorlevel 1 (
-  start "" notepad.exe "app\personalizar-diseno.css"
-) else (
-  start "" code "app\personalizar-diseno.css"
-)
